ae37bd46022b6f7b57b47d3e2525c7f4c9f6b309
Chun Hin D. Tang <dtang955472@gmail.com>
2021-05-24 12:56:02 -0400

    Pages component route setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = (props) => {
-  return <h1>Footer Component</h1>
+  return (
+    <nav>
+      
+    </nav>
+  )
 }
 
 export default Footer
--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -2,7 +2,77 @@ import React from 'react'
 import {Route, Link, Switch} from 'react-router-dom'
 
 const Pages = (props) => {
-  return <h1>Pages</h1>
+  return (
+    <div className='pages'>
+      <main>
+        <Switch>
+          <Route
+            exact
+            path="/home"
+            render={(rp) => (
+              <Home 
+                {...rp}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/menu"
+            render={(rp) => (
+              <Menu 
+                {...rp}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/product"
+            render={(rp) => (
+              <Product 
+                {...rp}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/productinfo"
+            render={(rp) => (
+              <ProductInfo 
+                {...rp}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/profile"
+            render={(rp) => (
+              <Profile 
+                {...rp}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/about"
+            render={(rp) => (
+              <About 
+                {...rp}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/order"
+            render={(rp) => (
+              <Order 
+                {...rp}
+              />
+            )}
+          />
+        </Switch>
+      </main>
+    </div>
+  )
 }
 
 export default Pages

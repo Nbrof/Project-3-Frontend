77e4461927ff3f2f7fb1a2c0cc2ee98162def37e
Chun Hin D. Tang <dtang955472@gmail.com>
2021-05-24 12:23:10 -0400

    creating and basic setup of components

---
+++ src/components/pages/Pages.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Route, Link, Switch} from 'react-router-dom'
+
+const Pages = (props) => {
+  return <h1>Pages</h1>
+}
+
+export default Pages
---
+++ src/components/pages/page/More.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const More = (props) => {
+  return <h1>More Page</h1>
+}
+
+export default More

e4f5101578d66ed38d8fc59d4046193ff8a10091
Chun Hin D. Tang <dtang955472@gmail.com>
2021-05-25 11:05:52 -0400

    added offer icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,33 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = (props) => {
   return (
     <nav>
-      <Link to='/menu'>
-        <FontAwesomeIcon icon='th-large' size='lg'/>
+      <Link to="/menu">
+        <FontAwesomeIcon icon="th-large" size="lg" />
         <p>Menu</p>
       </Link>
-      <Link to='/home'>
-        <FontAwesomeIcon icon='home' size='lg'/>
+      <Link to="/offer">
+        <FontAwesomeIcon icon="ice-cream" size="lg" />
+        <p>Offer</p>
+      </Link>
+      <Link to="/home">
+        <FontAwesomeIcon icon="home" size="lg" />
         <p>Home</p>
       </Link>
-      <Link to='/profile'>
-        <FontAwesomeIcon icon='user' size='lg'/>
+      <Link to="/profile">
+        <FontAwesomeIcon icon="user" size="lg" />
         <p>Profile</p>
       </Link>
-      <Link to='/more'>
-        <FontAwesomeIcon icon='bars' size='lg'/>
+      <Link to="/more">
+        <FontAwesomeIcon icon="bars" size="lg" />
         <p>More</p>
       </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

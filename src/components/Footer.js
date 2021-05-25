7f5e6189f01792fa5aa0d140ff00fbf1cc08e8d9
cphork <christina.phork@gmail.com>
2021-05-25 10:07:38 -0500

    chnages to icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,24 +5,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = (props) => {
   return (
+    <>
     <nav>
+      <div className='comp-btns'>
       <Link to='/menu'>
         <FontAwesomeIcon icon='th-large' size='lg'/>
         <p>Menu</p>
       </Link>
-      <Link to='/home'>
-        <FontAwesomeIcon icon='home' size='lg'/>
-        <p>Home</p>
-      </Link>
-      <Link to='/profile'>
-        <FontAwesomeIcon icon='user' size='lg'/>
-        <p>Profile</p>
-      </Link>
+      
       <Link to='/more'>
         <FontAwesomeIcon icon='bars' size='lg'/>
         <p>More</p>
       </Link>
+
+      <Link to='/home'>
+        <FontAwesomeIcon icon='home' size='lg'/>
+        <p>Home</p>
+      </Link>
+
+      <Link to='/profile'>
+        <FontAwesomeIcon icon='user' size='lg'/>
+        <p>Profile</p>
+      </Link>
+
+      
+      </div>
+
+
+      
+
+      
+      
+     
     </nav>
+    </>
   )
 }
 

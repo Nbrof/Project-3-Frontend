f597ffe7dee512b041eb4785b94e6e54844e1a1d
cphork <christina.phork@gmail.com>
2021-05-25 15:47:33 -0500

    added symbol

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -11,11 +11,11 @@ const Menu = (props) => {
   
     <div className='menu-btns'>
     <div className='red-box'></div>
-    
+
     <Link to={'/menu'}>
     <img className='icecreams-button' src='https://res.cloudinary.com/dejg3dz16/image/upload/v1621962157/scale_ksqcjj.jpg' alt='ice creams' />
     <button className='icecream-btn'>Cream</button>
-    <button class="button button5">></button>
+    <button class="button button5">&#62;</button>
     </Link>
     
     
@@ -24,7 +24,7 @@ const Menu = (props) => {
     <Link to={'/menu'}>
       <img className='floats-button'src='https://res.cloudinary.com/dejg3dz16/image/upload/v1621963185/4be670890df07c2cba3d457fb162edb1--soda-drink-strawberry-ice-cream-cake_q4luze.jpg' alt='ice cream floats' />
       <button className='floats-btn'>Floats</button>
-      <button class="button button5">></button>
+      <button class="button button5">&#62;</button>
     </Link>
     
 
@@ -33,7 +33,7 @@ const Menu = (props) => {
     <Link to={'/offer'}>
       <img className='offer-button' src='https://res.cloudinary.com/dejg3dz16/image/upload/v1621962761/5b19b1fcaa39cfeb3413dc0f_vopdfj.webp' alt='flavors of the month'/>
       <button className='offer-btn2'>Offers</button>
-      <button class="button button5">></button>
+      <button class="button button5">&#62;</button>
     </Link>
     
     </div>

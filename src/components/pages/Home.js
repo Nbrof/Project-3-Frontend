9a74a779e417887032581f5303701c797b161200
Chun Hin D. Tang <dtang955472@gmail.com>
2021-05-27 11:53:17 -0400

    home page added parlours

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,11 +2,32 @@ import React from "react";
 import "./css/Home.css";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Home = (props) => {
+  const parlours = props.parlours.map((parlour, index) => {
+    return (
+      <div className="parlour" key={index}>
+        <Link 
+          to='/products'
+          onClick={() => props.handleFilter(props.iceCreams, 'all')}
+        >
+          <img src={parlour.image} alt="parlour" />
+          <h2>{parlour.name}</h2>
+          <div className="rating">
+            <FontAwesomeIcon className="star" icon="star" />
+            <span>4</span>
+          </div>
+        </Link>
+      </div>
+    );
+  });
+
   return (
-    <body className='home-bg2'>
-    <h1>HOME</h1>
+    <body className="home-bg2 home">
+      <h1>HOME</h1>
+      {parlours}
     </body>
-  )
-  }
+  );
+};
 export default Home;

d67bfbabc1f6d09d16fe151ea51932fc8df08628
Chun Hin D. Tang <dtang955472@gmail.com>
2021-05-28 12:37:50 -0400

    order and confirm pages fixes

--- src/components/pages/Confirm.js
+++ src/components/pages/Confirm.js
@@ -1,29 +1,34 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from 'react-router-dom';
 import "./css/Order.css";
 
-const Confirm = props => {
+const Confirm = (props) => {
   return (
-    <>
-    <div className="popup-box">
-      <div className="box">
-        <span className="close-icon" onClick={props.handleClose}>x</span>
-        {props.content}
-      </div>
-    </div>
+    // <>
+    // <div className="popup-box">
+    //   <div className="box">
+    //     <span className="close-icon" onClick={props.handleClose}>x</span>
+    //     {props.content}
+    //   </div>
+    // </div>
+    // </>
 
-
-{/* <div className='confirm'>
-      <img src='https://res.cloudinary.com/dvxvez8mj/image/upload/v1622154473/Project%203/confirm_image_jshihg.png' alt=''/>
+    <div className="confirm">
+      <img
+        src="https://res.cloudinary.com/dvxvez8mj/image/upload/v1622154473/Project%203/confirm_image_jshihg.png"
+        alt=""
+      />
       <h1>Thank You!å</h1>
       <h2>for your order</h2>
-      <p>Your Order is now being processed. We will let you know once the order is picked from the outlet. Check the status of your Order</p>
-      <Link to='/home'>
+      <p>
+        Your Order is now being processed. We will let you know once the order
+        is picked from the outlet. Check the status of your Order
+      </p>
+      <Link to="/home">
         <button>Back to Home</button>
       </Link>
-    </div> */}
-    </>
+    </div>
   );
 };
- 
+
 export default Confirm;
